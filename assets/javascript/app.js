dc9a6d1de24192258e891eeaab101f35ebf6c5b2
Branford Harris <branfordharris@Branfords-MacBook-Pro.local>
2017-11-03 15:51:30 -0500

    added calculations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,42 @@
-
-var startDate;
-var monthsWorked;
-var name = "";
-var role = "";
-var monthlyRate;
-var totalBilled;
-
-
 $(document).ready(function(){
+//declare variables
+var firstTrain;
+var nextArrival;
+var destination = "";
+var name = "";
+var frequency;
+var minutesAway;
 	 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyB8MolB6enfH0M9lKhrs5Db1_BaAQrnO5g",
-    authDomain: "employee-management-138b3.firebaseapp.com",
-    databaseURL: "https://employee-management-138b3.firebaseio.com",
-    projectId: "employee-management-138b3",
-    storageBucket: "employee-management-138b3.appspot.com",
-    messagingSenderId: "983041657173"
+    apiKey: "AIzaSyDXSCsGNMu9TnUa5xdU73jSPSelimpvHFU",
+    authDomain: "choo-choo-4a8fd.firebaseapp.com",
+    databaseURL: "https://choo-choo-4a8fd.firebaseio.com",
+    projectId: "choo-choo-4a8fd",
+    storageBucket: "choo-choo-4a8fd.appspot.com",
+    messagingSenderId: "492041465496"
   };
   firebase.initializeApp(config);
   
   var database = firebase.database();
 
-
+  	//on Click event
 	$("#submit").on("click", function(event){
 		event.preventDefault();
+		//empty table
 		$("#tableBody").empty();
-
+		//get values from form
 		name = $("#nameInput").val().trim();
-		role = $("#roleInput").val().trim();
-		console.log(name, role);
-
+		destination = $("#destinationInput").val().trim();
+		frequency = $("#frequencyInput").val().trim();
+		//run calculations function on variables
 		calculations();
-
+		//push varibles to firebase
 		database.ref().push({
 			name: name,
-			role: role,
-			startDate: startDate,
-			monthsWorked: monthsWorked,
-			monthlyRate: monthlyRate,
-			totalBilled: totalBilled
+			destination: destination,
+			frequency: frequency,
+			nextArrival: nextArrival,
+			minutesAway: minutesAway
 
 		});//end of push()
 
@@ -47,46 +45,42 @@ $(document).ready(function(){
 
 	//on Database Change add elements to table
 	database.ref().on("value", function(snapshot) {
+		//for each entry add values to html table
 		snapshot.forEach(function(childSnapshot) {
 			var newTableRow = $("<tr>");
 			var nameData = $("<td>");
-			var roleData = $("<td>");
-			var startDateData = $("<td>");
-			var monthsWorkedData = $("<td>");
-			var monthlyRateData = $("<td>");
-			var totalBilledData = $("<td>");
+			var destinationData = $("<td>");
+			var frequencyData = $("<td>");
+			var nextArrivalData = $("<td>");
+			var minutesAwayData = $("<td>");
 			nameData.html(childSnapshot.val().name);
-			roleData.html(childSnapshot.val().role);
-			startDateData.html(childSnapshot.val().startDate);
-			monthsWorkedData.html(childSnapshot.val().monthsWorked);
-			monthlyRateData.html(childSnapshot.val().monthlyRate);
-			totalBilledData.html(childSnapshot.val().totalBilled);
-			newTableRow.append(nameData, roleData, startDateData, monthsWorkedData, monthlyRateData, totalBilledData);
+			destinationData.html(childSnapshot.val().destination);
+			frequencyData.html(childSnapshot.val().frequency);
+			nextArrivalData.html(childSnapshot.val().nextArrival);
+			minutesAwayData.html(childSnapshot.val().minutesAway);
+			newTableRow.append(nameData,destinationData,frequencyData,nextArrivalData,minutesAwayData);
 			$("#tableBody").append(newTableRow);
 		});
 	});
 
 
 	function calculations(){
-
-		startDate = $("#startDateInput").val().trim();
-		var dateSplit = startDate.split('/');
-		var formattedDate = dateSplit[1]+ '/' + dateSplit[0]+ '/'+dateSplit[2];
-		var newDate = moment(new Date(formattedDate));
-
-		var todaysDate = moment(new Date());
-		monthsWorked = todaysDate.diff(newDate,'months');
-
-		monthlyRate = $("#monthlyRateInput").val().trim();
-		totalBilled = monthlyRate * monthsWorked;
-
-		console.log("Start Date: " + newDate);
-		console.log("Today's Date: " + todaysDate);
-		console.log("Months Worked: " + monthsWorked + " months");
-		console.log("Months Rate: " + monthlyRate);
-		console.log("Total Billed: " + totalBilled);
-
-
+		//Grab first train value
+		firstTrain = $("#firstTrainInput").val().trim();
+		//change it to moment obj and subtract 1 year so it is in the future
+		var startTime = moment(firstTrain, "HH:mm").subtract(1, "years").format("HH:mm");
+		//grab current time
+		var now = moment();
+		//change to now to moment object formated "HH:mm"
+		now = moment(now, "HH:mm").format("HH:mm");
+		//Calc the differece between now and the first train
+		var timeDifference = moment().diff(moment(startTime, "HH:mm"), "minutes");
+		//Get the remainder fo the timeDifference and the frequency of the train
+		var minRemainder = timeDifference % frequency;
+		//find minutes away by subtracting the frequency by the current remainder of minutes
+		minutesAway = frequency - minRemainder;
+		//change arrival to string format "HH:mm"
+		nextArrival = moment(moment().add(minutesAway, "minutes")).format("HH:mm");
 	}//end of calculations()
 
 });//end document.ready()
